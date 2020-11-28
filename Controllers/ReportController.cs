2bb0ae2dab67c52a5d7261851d845fee40db94c4
JATNIEL FIGUEROA <jfigueroa60982@ufide.ac.cr>
2020-11-27 19:00:59 -0600

    PruebasReport

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgramacion.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Print() {
+
+            return View();
+        }
+    }
+}
